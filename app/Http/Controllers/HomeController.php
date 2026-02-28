2bfc1de9744c6f7e7d3164f8eba6fc2a22b2833d
Andrew Krzynowek <egamipeaks@gmail.com>
2026-02-28 16:47:35 -0600

    Order releases on the homepage by date

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends Controller
         $releases = $artist->releases()
             ->public()
             ->with('coverAsset')
+            ->latest('release_date')
             ->get();
 
         return view('home', compact('artist', 'releases'));
--- tests/Feature/HomePageTest.php
+++ tests/Feature/HomePageTest.php
@@ -21,6 +21,23 @@ it('shows public release titles on the homepage', function () {
         ->assertSee('Public Album');
 });
 
+it('shows releases ordered by release date descending', function () {
+    $artist = Artist::factory()->create();
+
+    $older = Release::factory()->for($artist)->public()->create([
+        'title' => 'Older Album',
+        'release_date' => now()->subYear(),
+    ]);
+    $newer = Release::factory()->for($artist)->public()->create([
+        'title' => 'Newer Album',
+        'release_date' => now(),
+    ]);
+
+    $this->get(route('home'))
+        ->assertSuccessful()
+        ->assertSeeInOrder(['Newer Album', 'Older Album']);
+});
+
 it('does not show draft release titles on the homepage', function () {
     $artist = Artist::factory()->create();
     Release::factory()->for($artist)->draft()->create(['title' => 'Hidden Draft']);
