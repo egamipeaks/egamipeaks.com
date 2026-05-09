c1dedff30e013b05934ae2c0db0bc6e82b59ad79
Andrew Krzynowek <egamipeaks@gmail.com>
2026-05-08 22:45:13 -0500

    display all releases

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,20 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Artist;
+use App\Models\Release;
 use Illuminate\Contracts\View\View;
 
 class HomeController extends Controller
 {
     public function __invoke(): View
     {
-        $artist = Artist::query()->with('heroImage')->firstOrFail();
-
-        $releases = $artist->releases()
+        $releases = Release::query()
             ->public()
             ->with('coverAsset')
             ->latest('release_date')
             ->get();
 
-        return view('home', compact('artist', 'releases'));
+        return view('home', compact('releases'));
     }
 }
